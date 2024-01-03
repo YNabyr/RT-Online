1d8b21667cb6223b9f7050e3a7f8275431b9da99
YNabyr <yusanbymm4@gmail.com>
2024-01-03 14:47:33 +0700

    add Controller

--- lib/features/dashboard/bindings/dashboard_binding.dart
+++ lib/features/dashboard/bindings/dashboard_binding.dart
@@ -1,9 +1,11 @@
 import 'package:get/get.dart';
+import 'package:venturo_core/features/beranda/controllers/image_slider_controller.dart';
 import 'package:venturo_core/features/dashboard/controllers/dashboard_controller.dart';
 
 class DashboardBinding extends Bindings {
   @override
   void dependencies() {
     Get.put(DashboardController());
+    Get.put(ImageSliderController());
   }
 }
